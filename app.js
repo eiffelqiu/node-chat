a8166df2fedb7c979f60adb5ff151703aadf196b
eiffel qiu <eiffelqiu@qq.com>
2014-11-19 22:24:44 +0800

    fix development mode

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require('express'), 
     http = require('http'), 
-    path = require('path')
-    io = require('socket.io');
+    path = require('path'),
+    io = require('socket.io'),
+    errorhandler = require('errorhandler');
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.errorHandler());
+
+if (app.get('env') === 'development') {
+  app.use(errorhandler());
+}
 
 // Set up express
 var server = http.createServer(app).listen(app.get('port'), function(){
@@ -22,18 +26,15 @@ io.on('connection', function (socket) {
     
     // Set the nickname property for a given client
     socket.on('nick', function(nick) {
-        //io.set('nickname', nick);
       socket.nickname = nick;
     });
 
     // Relay chat data to all clients
     socket.on('chat', function(data) {
-
             var payload = {
                 message: data.message,
                 nick: socket.nickname
             };
-
             socket.emit('chat',payload);
             socket.broadcast.emit('chat', payload);
     });
