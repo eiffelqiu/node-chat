b464685662cdefe3b6d0841019e57ed89c881ab1
eiffelqiu <eiffelqiu@gmail.com>
2015-04-06 19:01:06 +0800

    add local socket.io.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express = require('express'), 
-    http = require('http'), 
-    path = require('path'),
-    io = require('socket.io'),
-    errorhandler = require('errorhandler');
+var express = require('express'),
+	http = require('http'),
+	path = require('path'),
+	io = require('socket.io'),
+	errorhandler = require('errorhandler');
 
 var app = express();
 
@@ -10,32 +10,32 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
 
 if (app.get('env') === 'development') {
-  app.use(errorhandler());
+	app.use(errorhandler());
 }
 
 // Set up express
-var server = http.createServer(app).listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
+var server = http.createServer(app).listen(app.get('port'), function() {
+	console.log("Express server listening on port " + app.get('port'));
 });
 
 // Set up socket.io
 var io = require('socket.io').listen(server);
 
 // Handle socket traffic
-io.on('connection', function (socket) {
-    
-    // Set the nickname property for a given client
-    socket.on('nick', function(nick) {
-      socket.nickname = nick;
-    });
+io.on('connection', function(socket) {
 
-    // Relay chat data to all clients
-    socket.on('chat', function(data) {
-            var payload = {
-                message: data.message,
-                nick: socket.nickname
-            };
-            socket.emit('chat',payload);
-            socket.broadcast.emit('chat', payload);
-    });
+	// Set the nickname property for a given client
+	socket.on('nick', function(nick) {
+		socket.nickname = nick;
+	});
+
+	// Relay chat data to all clients
+	socket.on('chat', function(data) {
+		var payload = {
+			message: data.message,
+			nick: socket.nickname
+		};
+		socket.emit('chat', payload);
+		socket.broadcast.emit('chat', payload);
+	});
 });
